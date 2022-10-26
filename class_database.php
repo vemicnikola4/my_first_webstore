aa2c6f85f9044ffca464bdab9cb340108217ab4b
vemicnikola4 <vemicnikola4@gmail.com>
2022-10-26 12:33:57 +0200

    added 3 files class_database, index.php, log_in.php

---
+++ class_database.php
@@ -0,0 +1,46 @@
+<?php
+class Database{
+    public $conn;
+    function __construct($database){
+        $this->conn = new mysqli('localhost','root','',$database);
+        if ($this->conn->connect_error){
+            echo "JDK connection error " . $this->conn->connect_error;
+        }else{
+            echo "<p>Connected</p>";
+        }
+    }
+    function select($select){
+        $data = $this-> conn -> query($select);
+        if ( $data == false){
+            return ['sucssesful'=>false,'message'=> $this->conn->error];
+        }else{
+            $array= $data->fetch_all(MYSQLI_ASSOC);
+            return['sucssesful'=>true,'array'=>$array];
+        }
+    }
+    function comand($comand){
+        $execute = $this->conn->query($comand);
+        if  ($execute == false){
+            die("JDK " .$this->conn->error);
+        }else{
+           return "Sucssesfuly executed !";
+        }
+    }
+    function insert_user($email,$password,$name){
+        $email = $this -> conn-> real_escape_string($email);
+        $password = $this -> conn-> real_escape_string($password);
+        $name = $this -> conn-> real_escape_string($name);
+        $insert = $this->comand("INSERT INTO user (`email`, `password`, `name`) VALUES ('$email','$password','$name')");
+    }
+    function all_users(){
+        $all_users = $this->select("SELECT * FROM user");
+        if ($all_users['sucssesful'] == true){
+            return $all_users['array'];
+        }else{
+            die( "NEUSPESAN UPIT ". $all_users['message']);
+        }
+    }
+}
+$base = new Database('web_store');
+
+?>
