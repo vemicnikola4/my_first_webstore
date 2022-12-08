deea44b00de8cb53ed1ec3141ec77e31a2439bb4
vemicnikola4 <vemicnikola4@gmail.com>
2022-12-08 09:13:52 +0100

    Back to old version in log_in.php

--- log_in.php
+++ log_in.php
@@ -13,7 +13,11 @@ if (isset($_GET['action'])&& $_GET['action'] == "log_in"){
     if ( isset ($_GET['email']) && isset($_GET['password'])){
         $email = $_GET['email'];
         $password = $_GET['password'];
-        if ( $email !== "" || $password !== ""){
+        if ( $email == "" || $password == ""){
+            echo "<div style='padding-left:50px;padding-top:50px'>";
+            echo "<p>ADD EMAIL AND PASWORD</p>";
+            echo "</div>";
+        }else{
             $all_users = $base -> all_users();
             for ($i = 0 ; $i < count($all_users); $i++){
                 if ( $email == $all_users[$i]['email'] && $password == $all_users[$i]['password'] ){
@@ -30,34 +34,32 @@ if (isset($_GET['action'])&& $_GET['action'] == "log_in"){
        
         }
         echo "<div class='form_div'>";
-        echo "<div class='log_in_div'>";
         echo "<p>INVALID EMAIL OR PASSWORD TRY AGAIN</p>";
         echo  '<form action="log_in.php">';
         echo  '<input type="hidden" name="action" value="log_in">';
-        echo  '<input class="text" type="text" name="email" placeholder="email">';
-        echo  '<input class="text" type="text" name="password" placeholder="password">';
+        echo  '<input type="text" name="email" placeholder="email">';
+        echo  '<input type="text" name="password" placeholder="password">';
         echo "<br>";
-        echo  '<p>Remember me on this computer <input type="checkbox" name="remember"></p>';
+        echo "Remember me on this computer";
+        echo  '<input type="checkbox" name="remember">';
         echo "<br>";
-        echo  '<input class="button" type="submit" value="log_in">';
+        echo  '<input type="submit" value="log_in">';
         echo  '</form>';
-        echo "</div>";
         echo "</div>";
     }
 }
 if (isset($_GET['action'])&& $_GET['action'] !== "log_in"){
     echo "<div class='form_div'>";
-    echo "<div class='log_in_div'>";
     echo  '<form action="log_in.php">';
     echo  '<input type="hidden" name="action" value="log_in">';
-    echo  '<input class="text" type="text" name="email" placeholder="email">';
-    echo  '<input class="text" type="text" name="password" placeholder="password">';
+    echo  '<input type="text" name="email" placeholder="email">';
+    echo  '<input type="text" name="password" placeholder="password">';
     echo  '<br>';
-    echo  '<p>Remember me on this computer <input type="checkbox" name="remember"></p>';
+    echo  ' Remember me on this computer ';
+    echo  '<input type="checkbox" name="remember">';
     echo  '<br>';
-    echo  '<input class="button" type="submit" value="log_in">';
+    echo  '<input type="submit" value="log_in">';
     echo  '</form>';
-    echo "</div>";
     echo "</div>";
 }
 echo bottom_footer();
